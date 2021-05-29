4d75bd2091521fc3dbd586dffd9ec00945469b56
Henrique Boneto <h.boneto@outlook.com>
2021-05-29 15:34:16 -0300

    class name

--- clients/models.py
+++ clients/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class client(models.Model):
+class Client(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(blank=False, max_length=40)
     id_number = models.CharField(max_length=11, unique=True)
