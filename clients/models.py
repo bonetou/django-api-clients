77d95b4c916934561aec6bf2d1fba4eb98e5c5e8
Henrique Boneto <h.boneto@outlook.com>
2021-05-29 15:30:02 -0300

    client model

--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class client(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(blank=False, max_length=40)
+    id_number = models.CharField(max_length=11, unique=True)
+    phone_number = models.CharField(max_length=14)
+    active = models.BooleanField()
+
+    def __str__(self) -> str:
+        return self.name
