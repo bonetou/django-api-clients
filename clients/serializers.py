72cdd1a087cba8623d5e2084cb3c499eb5b3bad7
Henrique Boneto <h.boneto@outlook.com>
2021-05-29 20:03:28 -0300

    validations

--- clients/serializers.py
+++ clients/serializers.py
@@ -11,3 +11,13 @@ class ClientSerializer(serializers.ModelSerializer):
         if id_number != 11:
             raise serializers.ValidationError("id_number must have 11 characters")
         return id_number
+
+    def validate_name(self, name):
+        if not name.isalpha():
+            raise serializers.ValidationError("name must be alphabetical")
+        return name
+
+    def validate_phone_number(self, number):
+        if len(number) < 11:
+            raise serializers.ValidationError("phone_number must have at least 11 characters")
+        return number
