812dcb8923d16d2c44fe8f8645785d2b8903f08b
Henrique Boneto <h.boneto@outlook.com>
2021-05-29 15:46:24 -0300

    admin model

--- clients/admin.py
+++ clients/admin.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from clients.models import Client
 
 class Clients(admin.ModelAdmin):
-    list_display = ('id', 'nome', 'email','cpf', 'rg', 'celular', 'ativo')
-    list_display_links = ('id', 'nome')
-    search_fields = ('nome',)
-    list_filter = ('ativo',)
-    list_editable = ('ativo',)
+    list_display = ('id', 'name', 'email','id_number', 'phone_number', 'active')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    list_filter = ('active',)
+    list_editable = ('active',)
     list_per_page = 25
 
 admin.site.register(Client, Clients)
