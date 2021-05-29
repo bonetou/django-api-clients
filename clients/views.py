b757195670480428eef8c7ab1008e2add0f5ae2f
Henrique Boneto <h.boneto@outlook.com>
2021-05-29 15:38:08 -0300

    client viewset

--- clients/views.py
+++ clients/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from clients.models import Client
+from clients.serializers import ClientSerializer
 
-# Create your views here.
+class ClientViewSet(viewsets.ModelViewSet):
+    '''List clients'''
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+
+
