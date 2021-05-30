f39c3df46416c88f56b19774ce6e654e4304a110
Henrique Boneto <h.boneto@outlook.com>
2021-05-29 22:18:00 -0300

    seach and filter

--- clients/views.py
+++ clients/views.py
@@ -1,14 +1,13 @@
 from rest_framework import viewsets, filters
 from clients.models import Client
 from clients.serializers import ClientSerializer
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters.rest_framework import DjangoFilterBackend, filterset
 
 class ClientViewSet(viewsets.ModelViewSet):
     '''List clients'''
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
-    filter_backends  = [DjangoFilterBackend, filters.OrderingFilter]
-    ordering_filters = ['name']
-
-    
-
+    filter_backends  = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
+    ordering_fields = ['name']
+    search_fields = ['name', 'cpf']
+    filterset_fields = ['active']
